dcdf8c1cb2ba452710dc4b670160825c43bca6f6
Mena Wahba <menawahba@gmail.com>
2021-02-27 16:42:10 -0500

    added the test functions

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,6 +1,15 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
-function Engineer() {
-
+const Employee = require("./Employee");
+class Engineer extends Employee {
+    constructor(name, email, id, github){
+        super(name, id, email);
+        this.github = github;
+    }
+    getRole(){
+        return "Engineer";
+    }
+    getGithub() {
+        return this.github;
+    }
 };
-
 module.exports = Engineer;
--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,6 +1,15 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
-function Intern() {
-
+const Employee = require("./Employee");
+class Intern extends Employee {
+    constructor(name, email, id, school) {
+        super(name, id, email);
+        this.school = school;
+    }
+    getRole() {
+        return "Intern";
+    }
+    getSchool(){
+        return this.school;
+    }
 };
-
 module.exports = Intern;
